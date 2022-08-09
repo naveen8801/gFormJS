5c390461c12da6b8b72653859ea90862ec9ffc77
naveen8801 <naveensharma10d@gmail.com>
2022-08-09 15:57:26 +0530

    added validation for no of question

---
+++ FormFormat.js
@@ -0,0 +1,35 @@
+let a = {
+	formId: "",
+	info: {
+		title: "",
+		documentTitle: "",
+		description: "",
+	},
+	items: [
+		{
+			itemId: "",
+			title: "",
+			description: "",
+			questionItem: {
+				questionId: "",
+				required: true,
+				choiceQuestion: {
+					type: "RADIO CHECKBOX DROP_DOWN",
+					options: [
+						{
+							value: "",
+						},
+					],
+					shuffle: false,
+				},
+				textQuestion: {
+					paragraph: true,
+				},
+			},
+		},
+	],
+	settings: {},
+	revisionId: "",
+	responderUri: "",
+	linkedSheetId: "",
+};
--- ui.js
+++ ui.js
@@ -7,12 +7,75 @@ const Gradient = require("ink-gradient");
 const BigText = require("ink-big-text");
 const { default: SelectInput } = require("ink-select-input");
 const GoogleAuth = require("./core/GoogleAuth");
+const { default: TextInput } = require("ink-text-input");
+const { default: Spinner } = require("ink-spinner");
+
+const QuestionJourney = ({ quesCount }) => {
+	return <Text>{quesCount}</Text>;
+};
+
+const SeeResponse = () => {
+	return <Text>Responses</Text>;
+};
+
+const CreateForm = ({ mainStep, setMainStep }) => {
+	const [quesCount, setQuesCount] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [showPrompt, setShowPrompt] = useState(true);
+	const [error, setError] = useState({ err: false, msg: "" });
+	const onSubmitHandler = () => {
+		setLoading(true);
+		setShowPrompt(false);
+		if (
+			isNaN(quesCount) ||
+			parseInt(quesCount) > 5 ||
+			parseInt(quesCount) === 0
+		) {
+			setLoading(false);
+			setShowPrompt(true);
+			setError({ err: true, msg: "Plese input a number between 1 and 5" });
+			return;
+		} else {
+			setLoading(true);
+			setShowPrompt(false);
+			setError({ err: false, msg: "" });
+			setMainStep(2);
+		}
+	};
+	return (
+		<>
+			{mainStep === 1 ? (
+				<>
+					{showPrompt ? (
+						<>
+							<Text color="green">Enter No. of Questions :</Text>
+							<TextInput
+								value={quesCount}
+								onChange={setQuesCount}
+								onSubmit={onSubmitHandler}
+							/>
+						</>
+					) : null}
+					{loading ? (
+						<Text color="green">
+							<Spinner type="dots" /> Loading...
+						</Text>
+					) : null}
+					{error.err ? <Text color="red">{error.msg}</Text> : null}
+				</>
+			) : (
+				<QuestionJourney quesCount={parseInt(quesCount)} />
+			)}
+		</>
+	);
+};
 
 const App = ({ login = false }) => {
 	const [currentStage, setCurrentStage] = useState(0);
-	git s
+	const [mainStep, setMainStep] = useState(0);
 	const handleSelect = (item) => {
 		setCurrentStage(item.value);
+		setMainStep(1);
 	};
 
 	const items = [
@@ -34,9 +97,9 @@ const App = ({ login = false }) => {
 			{currentStage == 0 ? (
 				<SelectInput items={items} onSelect={handleSelect} />
 			) : currentStage === 1 ? (
-				<Text>Creating</Text>
+				<CreateForm mainStep={mainStep} setMainStep={setMainStep} />
 			) : (
-				<Text>Looking</Text>
+				<SeeResponse mainStep={mainStep} setMainStep={setMainStep} />
 			)}
 		</Box>
 	);
